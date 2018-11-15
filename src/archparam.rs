6364fac1bba14d72058ed13676b4b20cbab3dcc3
bluss <bluss@users.noreply.github.com>
2018-11-15 20:33:59 +0100

    MAINT: Update copyright headers

--- src/archparam.rs
+++ src/archparam.rs
@@ -1,4 +1,4 @@
-// Copyright 2016 bluss
+// Copyright 2016 - 2018 Ulrik Sverdrup "bluss"
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT license

1ef336ae697c2f3ec22825d612a6dd0450f42e6e
bluss <bluss@users.noreply.github.com>
2018-12-07 21:31:46 +0100

    TEST: Change test environment variable to use MMTEST_FEATURE=avx etc
    Instead of excluding features to test, *include* them.
    
    If you set MMTEST_FEATURE=avx, only the "avx" feature ever tests true at
    runtime. We include "TEST" in the environment variable name so that it
    is clear. "MM" is for matrixmultiply.
    
    Also add a test that can be optionally enabled, to ensure we test
    a feature.

--- src/x86/macros.rs
+++ src/x86/macros.rs
@@ -1,16 +1,20 @@
 #![cfg(any(target_arch="x86", target_arch="x86_64"))]
 
-macro_rules! compile_env_enabled {
-    ($($name:tt)*) => {
-        !option_env!($($name)*).unwrap_or("").is_empty()
+macro_rules! compile_env_matches_or_is_empty {
+    ($envvar:tt, $feature_name:tt) => {
+        (match option_env!($envvar) {
+            None => true,
+            Some(v) => v == $feature_name
+        })
     }
 }
 
 macro_rules! is_x86_feature_detected_ {
     ($name:tt) => {
-        // for testing purposes, we can disable a feature at compile time by
-        // setting MMNO_avx=1 etc.
-        !compile_env_enabled!(concat!("MMNO_", $name)) && is_x86_feature_detected!($name)
+        // for testing purposes, we can make sure only one specific feature
+        // is enabled by setting MMTEST_FEATURE=featurename (all others
+        // disabled). This does not force it to be detected, it must also be.
+        compile_env_matches_or_is_empty!("MMTEST_FEATURE", $name) && is_x86_feature_detected!($name)
     }
 }
 

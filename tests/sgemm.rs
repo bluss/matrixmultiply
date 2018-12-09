1ae7f38a2a3a17f0268f36a2bc225773ce12c1d6
Richard Janis Goldschmidt <janis.beckert@gmail.com>
2018-12-09 13:26:20 +0100

    First shot at generalizing and fixing the integration tests

--- tests/sgemm.rs
+++ tests/sgemm.rs
@@ -1,7 +1,11 @@
 extern crate itertools;
 extern crate matrixmultiply;
 
-use matrixmultiply::{sgemm, dgemm}; //, igemm};
+use matrixmultiply::{
+    sgemm,
+    dgemm,
+    i8gemm,
+};
 
 use itertools::Itertools;
 use itertools::{
@@ -11,93 +15,129 @@ use itertools::{
 };
 use std::fmt::{Display, Debug};
 
-trait Float : Copy + Display + Debug + PartialEq {
-    fn zero() -> Self;
-    fn one() -> Self;
-    fn from(x: i64) -> Self;
-    fn nan() -> Self;
-    fn is_nan(self) -> bool;
+trait GemmElement : Copy + Display + Debug + PartialEq {
+    // TODO: Provide default associated types once the following RFCs are merged and implemented:
+    // https://github.com/rust-lang/rfcs/pull/2532
+    // https://github.com/rust-lang/rust/issues/29661
+    // I.e., then we can do something like:
+    // type Output = Self;
+    //
+    // XXX: Is it somehow possible to already provide default impls for the _out functions in terms
+    // of the _in functions, where we assume that Input = Output?
+    type Output: Copy + Display + Debug + PartialEq;
+
+    fn zero_in() -> Self;
+    fn one_in() -> Self;
+    fn nan_in() -> Self;
+    fn from_in(x: i64) -> Self;
+    fn is_nan_in(Self) -> bool;
+
+    fn zero_out() -> Self::Output;
+    fn one_out() -> Self::Output;
+    fn nan_out() -> Self::Output;
+    fn from_out(x: i64) -> Self::Output;
+    fn is_nan_out(Self::Output) -> bool;
+
+    fn to_out(Self) -> Self::Output;
 }
 
-impl Float for f32 {
-    fn zero() -> Self { 0. }
-    fn one() -> Self { 1. }
-    fn from(x: i64) -> Self { x as Self }
-    fn nan() -> Self { 0./0. }
-    fn is_nan(self) -> bool { self.is_nan() }
+macro_rules! impl_gemm_element_f {
+    ($($t:ty),+) => {
+        $(
+        impl GemmElement for $t {
+            type Output = Self;
+
+            fn zero_in() -> Self { 0. }
+            fn one_in() -> Self { 1. }
+            fn from_in(x: i64) -> Self { x as Self }
+            fn nan_in() -> Self { 0./0. }
+            fn is_nan_in(var: Self) -> bool { var.is_nan() }
+
+            fn zero_out() -> Self::Output { 0. }
+            fn one_out() -> Self::Output { 1. }
+            fn from_out(x: i64) -> Self::Output { x as Self::Output }
+            fn nan_out() -> Self { 0./0. }
+            fn is_nan_out(var: Self::Output) -> bool { var.is_nan() }
+
+            fn to_out(var: Self) -> Self::Output {
+                var
+            }
+        }
+        )+
+    };
 }
 
-impl Float for f64 {
-    fn zero() -> Self { 0. }
-    fn one() -> Self { 1. }
-    fn from(x: i64) -> Self { x as Self }
-    fn nan() -> Self { 0./0. }
-    fn is_nan(self) -> bool { self.is_nan() }
-}
+impl_gemm_element_f!(f32, f64);
 
-impl Float for i32 {
-    fn zero() -> Self { 0 }
-    fn one() -> Self { 1 }
-    fn from(x: i64) -> Self { x as Self }
-    fn nan() -> Self { i32::min_value() } // hack
-    fn is_nan(self) -> bool { self == i32::min_value() }
-}
+impl GemmElement for i8 {
+    type Output = i16;
 
-trait Gemm : Sized {
-    unsafe fn gemm(
-        m: usize, k: usize, n: usize,
-        alpha: Self,
-        a: *const Self, rsa: isize, csa: isize,
-        b: *const Self, rsb: isize, csb: isize,
-        beta: Self,
-        c: *mut Self, rsc: isize, csc: isize);
-}
+    fn zero_in() -> Self { 0 }
+    fn one_in() -> Self { 1 }
+    fn from_in(x: i64) -> Self { x as Self }
+    fn nan_in() -> Self { i8::min_value() } // hack
+    fn is_nan_in(var: Self) -> bool { var == Self::nan_in() }
 
-impl Gemm for f32 {
-    unsafe fn gemm(
-        m: usize, k: usize, n: usize,
-        alpha: Self,
-        a: *const Self, rsa: isize, csa: isize,
-        b: *const Self, rsb: isize, csb: isize,
-        beta: Self,
-        c: *mut Self, rsc: isize, csc: isize) {
-        sgemm(
-            m, k, n,
-            alpha,
-            a, rsa, csa,
-            b, rsb, csb,
-            beta,
-            c, rsc, csc)
+    fn zero_out() -> Self::Output { 0 }
+    fn one_out() -> Self::Output { 1 }
+    fn from_out(x: i64) -> Self::Output { x as Self::Output }
+    fn nan_out() -> Self::Output { i16::min_value() } // hack
+    fn is_nan_out(var: Self::Output) -> bool { var == Self::nan_out() }
+
+    fn to_out(var: Self) -> Self::Output {
+        var as Self::Output
     }
 }
 
-// impl Gemm for i32 {
-//     unsafe fn gemm(
-//         m: usize, k: usize, n: usize,
-//         alpha: Self,
-//         a: *const Self, rsa: isize, csa: isize,
-//         b: *const Self, rsb: isize, csb: isize,
-//         beta: Self,
-//         c: *mut Self, rsc: isize, csc: isize) {
-//         igemm(
-//             m, k, n,
-//             alpha,
-//             a, rsa, csa,
-//             b, rsb, csb,
-//             beta,
-//             c, rsc, csc)
-//     }
-// }
+trait Gemm : Sized {
+    type Output;
 
-impl Gemm for f64 {
     unsafe fn gemm(
         m: usize, k: usize, n: usize,
-        alpha: Self,
+        alpha: Self::Output,
         a: *const Self, rsa: isize, csa: isize,
         b: *const Self, rsb: isize, csb: isize,
-        beta: Self,
-        c: *mut Self, rsc: isize, csc: isize) {
-        dgemm(
+        beta: Self::Output,
+        c: *mut Self::Output, rsc: isize, csc: isize);
+}
+
+macro_rules! impl_gemm_f {
+    ($(($t:ty, $f:ident)),+) => {
+        $(
+        impl Gemm for $t {
+            type Output = Self;
+            unsafe fn gemm(
+                m: usize, k: usize, n: usize,
+                alpha: Self,
+                a: *const Self, rsa: isize, csa: isize,
+                b: *const Self, rsb: isize, csb: isize,
+                beta: Self,
+                c: *mut Self, rsc: isize, csc: isize) {
+                $f(
+                    m, k, n,
+                    alpha,
+                    a, rsa, csa,
+                    b, rsb, csb,
+                    beta,
+                    c, rsc, csc)
+            }
+        }
+        )+
+    };
+}
+
+impl_gemm_f!((f32, sgemm), (f64, dgemm));
+
+impl Gemm for i8 {
+    type Output = i16;
+    unsafe fn gemm(
+        m: usize, k: usize, n: usize,
+        alpha: i16,
+        a: *const Self, rsa: isize, csa: isize,
+        b: *const Self, rsb: isize, csb: isize,
+        beta: i16,
+        c: *mut i16, rsc: isize, csc: isize) {
+        i8gemm(
             m, k, n,
             alpha,
             a, rsa, csa,
@@ -109,105 +149,112 @@ impl Gemm for f64 {
 
 #[test]
 fn test_sgemm() {
-    test_gemm::<f32>();
+    test_gemm::<f32, _>();
 }
 #[test]
 fn test_dgemm() {
-    test_gemm::<f64>();
+    test_gemm::<f64, _>();
 }
 #[test]
 fn test_sgemm_strides() {
-    test_gemm_strides::<f32>();
+    test_gemm_strides::<f32, _>();
 }
 #[test]
 fn test_dgemm_strides() {
-    test_gemm_strides::<f64>();
+    test_gemm_strides::<f64, _>();
 }
 
-// #[test]
-// fn test_i32gemm_strides() {
-//     test_gemm_strides::<i32>();
-// }
+#[test]
+fn test_i8gemm_strides() {
+    test_gemm_strides::<i8, _>();
+}
 
-fn test_gemm_strides<F>() where F: Gemm + Float {
+fn test_gemm_strides<F, Tout>()
+    where F: Gemm<Output=Tout> + GemmElement<Output=Tout>,
+          Tout: Copy + Display + Debug + PartialEq
+{
     for n in 0..10 {
-        test_strides::<F>(n, n, n);
+        test_strides::<F, _>(n, n, n);
     }
     for n in (3..12).map(|x| x * 7) {
-        test_strides::<F>(n, n, n);
+        test_strides::<F, _>(n, n, n);
     }
 
-    test_strides::<F>(8, 12, 16);
-    test_strides::<F>(8, 0, 10);
+    test_strides::<F, _>(8, 12, 16);
+    test_strides::<F, _>(8, 0, 10);
 }
 
-fn test_gemm<F>() where F: Gemm + Float {
-    test_mul_with_id::<F>(4, 4, true);
-    test_mul_with_id::<F>(8, 8, true);
-    test_mul_with_id::<F>(32, 32, false);
-    test_mul_with_id::<F>(128, 128, false);
-    test_mul_with_id::<F>(17, 128, false);
+fn test_gemm<F, Tout>()
+    where F: Gemm<Output=Tout> + GemmElement<Output=Tout>,
+          Tout: Copy + Display + Debug + PartialEq
+{
+    test_mul_with_id::<F, _>(4, 4, true);
+    test_mul_with_id::<F, _>(8, 8, true);
+    test_mul_with_id::<F, _>(32, 32, false);
+    test_mul_with_id::<F, _>(128, 128, false);
+    test_mul_with_id::<F, _>(17, 128, false);
     for i in 0..12 {
         for j in 0..12 {
-            test_mul_with_id::<F>(i, j, true);
+            test_mul_with_id::<F, _>(i, j, true);
         }
     }
     /*
     */
-    test_mul_with_id::<F>(17, 257, false);
-    test_mul_with_id::<F>(24, 512, false);
+    test_mul_with_id::<F, _>(17, 257, false);
+    test_mul_with_id::<F, _>(24, 512, false);
     for i in 0..10 {
         for j in 0..10 {
-            test_mul_with_id::<F>(i * 4, j * 4, true);
+            test_mul_with_id::<F, _>(i * 4, j * 4, true);
         }
     }
-    test_mul_with_id::<F>(266, 265, false);
-    test_mul_id_with::<F>(4, 4, true);
+    test_mul_with_id::<F, _>(266, 265, false);
+    test_mul_id_with::<F, _>(4, 4, true);
     for i in 0..12 {
         for j in 0..12 {
-            test_mul_id_with::<F>(i, j, true);
+            test_mul_id_with::<F, _>(i, j, true);
         }
     }
-    test_mul_id_with::<F>(266, 265, false);
-    test_scale::<F>(0, 4, 4, true);
-    test_scale::<F>(4, 0, 4, true);
-    test_scale::<F>(4, 4, 0, true);
-    test_scale::<F>(4, 4, 4, true);
-    test_scale::<F>(19, 20, 16, true);
-    test_scale::<F>(150, 140, 128, false);
+    test_mul_id_with::<F, _>(266, 265, false);
+    test_scale::<F, Tout>(0, 4, 4, true);
+    test_scale::<F, Tout>(4, 0, 4, true);
+    test_scale::<F, Tout>(4, 4, 0, true);
+    test_scale::<F, Tout>(4, 4, 4, true);
+    test_scale::<F, Tout>(19, 20, 16, true);
+    test_scale::<F, Tout>(150, 140, 128, false);
 
 }
 
 /// multiply a M x N matrix with an N x N id matrix
 #[cfg(test)]
-fn test_mul_with_id<F>(m: usize, n: usize, small: bool)
-    where F: Gemm + Float
+fn test_mul_with_id<F, Tout>(m: usize, n: usize, small: bool)
+    where F: Gemm<Output=Tout> + GemmElement<Output=Tout>,
+          Tout: Copy + Display + Debug + PartialEq
 {
     let (m, k, n) = (m, n, n);
-    let mut a = vec![F::zero(); m * k]; 
-    let mut b = vec![F::zero(); k * n];
-    let mut c = vec![F::zero(); m * n];
+    let mut a = vec![F::zero_in(); m * k]; 
+    let mut b = vec![F::zero_in(); k * n];
+    let mut c = vec![F::zero_out(); m * n];
     println!("test matrix with id input M={}, N={}", m, n);
 
     for (i, elt) in a.iter_mut().enumerate() {
-        *elt = F::from(i as i64);
+        *elt = F::from_in(i as i64);
     }
     for i in 0..k {
-        b[i + i * k] = F::one();
+        b[i + i * k] = F::one_in();
     }
 
     unsafe {
         F::gemm(
             m, k, n,
-            F::one(),
+            F::one_out(),
             a.as_ptr(), k as isize, 1,
             b.as_ptr(), n as isize, 1,
-            F::zero(),
+            F::zero_out(),
             c.as_mut_ptr(), n as isize, 1,
             )
     }
     for (i, (x, y)) in a.iter().zip(&c).enumerate() {
-        if x != y {
+        if F::to_out(*x) != *y {
             if k != 0 && n != 0 && small {
                 for row in a.chunks(k) {
                     println!("{:?}", row);
@@ -228,33 +275,34 @@ fn test_mul_with_id<F>(m: usize, n: usize, small: bool)
 
 /// multiply a K x K id matrix with an K x N matrix
 #[cfg(test)]
-fn test_mul_id_with<F>(k: usize, n: usize, small: bool) 
-    where F: Gemm + Float
+fn test_mul_id_with<F, Tout>(k: usize, n: usize, small: bool) 
+    where F: Gemm<Output=Tout> + GemmElement<Output=Tout>,
+          Tout: Copy + Display + Debug + PartialEq
 {
     let (m, k, n) = (k, k, n);
-    let mut a = vec![F::zero(); m * k]; 
-    let mut b = vec![F::zero(); k * n];
-    let mut c = vec![F::zero(); m * n];
+    let mut a = vec![F::zero_in(); m * k]; 
+    let mut b = vec![F::zero_in(); k * n];
+    let mut c = vec![F::zero_out(); m * n];
 
     for i in 0..k {
-        a[i + i * k] = F::one();
+        a[i + i * k] = F::one_in();
     }
     for (i, elt) in b.iter_mut().enumerate() {
-        *elt = F::from(i as i64);
+        *elt = F::from_in(i as i64);
     }
 
     unsafe {
         F::gemm(
             m, k, n,
-            F::one(),
+            F::one_out(),
             a.as_ptr(), k as isize, 1,
             b.as_ptr(), n as isize, 1,
-            F::zero(),
+            F::zero_out(),
             c.as_mut_ptr(), n as isize, 1,
             )
     }
     for (i, (x, y)) in b.iter().zip(&c).enumerate() {
-        if x != y {
+        if F::to_out(*x) != *y {
             if k != 0 && n != 0 && small {
                 for row in a.chunks(k) {
                     println!("{:?}", row);
@@ -274,55 +322,56 @@ fn test_mul_id_with<F>(k: usize, n: usize, small: bool)
 }
 
 #[cfg(test)]
-fn test_scale<F>(m: usize, k: usize, n: usize, small: bool)
-    where F: Gemm + Float
+fn test_scale<F, Tout>(m: usize, k: usize, n: usize, small: bool)
+    where F: Gemm<Output=Tout> + GemmElement<Output=Tout>,
+          Tout: Copy + Display + Debug + PartialEq
 {
     let (m, k, n) = (m, k, n);
-    let mut a = vec![F::zero(); m * k]; 
-    let mut b = vec![F::zero(); k * n];
-    let mut c1 = vec![F::one(); m * n];
-    let mut c2 = vec![F::nan(); m * n];
+    let mut a = vec![F::zero_in(); m * k]; 
+    let mut b = vec![F::zero_in(); k * n];
+    let mut c1 = vec![F::one_out(); m * n];
+    let mut c2 = vec![F::nan_out(); m * n];
     // init c2 with NaN to test the overwriting behavior when beta = 0.
 
     for (i, elt) in a.iter_mut().enumerate() {
-        *elt = F::from(i as i64);
+        *elt = F::from_in(i as i64);
     }
     for (i, elt) in b.iter_mut().enumerate() {
-        *elt = F::from(i as i64);
+        *elt = F::from_in(i as i64);
     }
 
     unsafe {
         // C1 = 3 A B
         F::gemm(
             m, k, n,
-            F::from(3),
+            F::from_out(3),
             a.as_ptr(), k as isize, 1,
             b.as_ptr(), n as isize, 1,
-            F::zero(),
+            F::zero_out(),
             c1.as_mut_ptr(), n as isize, 1,
         );
 
         // C2 = A B 
         F::gemm(
             m, k, n,
-            F::one(),
+            F::one_out(),
             a.as_ptr(), k as isize, 1,
             b.as_ptr(), n as isize, 1,
-            F::zero(),
+            F::zero_out(),
             c2.as_mut_ptr(), n as isize, 1,
         );
         // C2 = A B + 2 C2
         F::gemm(
             m, k, n,
-            F::one(),
+            F::one_out(),
             a.as_ptr(), k as isize, 1,
             b.as_ptr(), n as isize, 1,
-            F::from(2),
+            F::from_out(2),
             c2.as_mut_ptr(), n as isize, 1,
         );
     }
     for (i, (x, y)) in c1.iter().zip(&c2).enumerate() {
-        if x != y || x.is_nan() || y.is_nan() {
+        if x != y || F::is_nan_out(*x) || F::is_nan_out(*y) {
             if k != 0 && n != 0 && small {
                 for row in a.chunks(k) {
                     println!("{:?}", row);
@@ -369,8 +418,9 @@ impl Default for Layout {
 
 
 #[cfg(test)]
-fn test_strides<F>(m: usize, k: usize, n: usize)
-    where F: Gemm + Float
+fn test_strides<F, Tout>(m: usize, k: usize, n: usize)
+    where F: Gemm<Output=Tout> + GemmElement<Output=Tout>,
+          Tout: Copy + Display + Debug + PartialEq
 {
     let (m, k, n) = (m, k, n);
 
@@ -383,15 +433,16 @@ fn test_strides<F>(m: usize, k: usize, n: usize)
     for elt in layouts_iter {
         let layouts = [elt[0], elt[1], elt[2], elt[3]];
         let (m0, m1, m2, m3) = multipliers_iter.next_tuple().unwrap();
-        test_strides_inner::<F>(m, k, n, [m0, m1, m2, m3], layouts);
+        test_strides_inner::<F, _>(m, k, n, [m0, m1, m2, m3], layouts);
     }
 }
 
 
-fn test_strides_inner<F>(m: usize, k: usize, n: usize,
+fn test_strides_inner<F, Tout>(m: usize, k: usize, n: usize,
                          stride_multipliers: [[usize; 2]; 4],
                          layouts: [Layout; 4])
-    where F: Gemm + Float
+    where F: Gemm<Output=Tout> + GemmElement<Output=Tout>,
+          Tout: Copy + Display + Debug + PartialEq
 {
     let (m, k, n) = (m, k, n);
 
@@ -401,16 +452,16 @@ fn test_strides_inner<F>(m: usize, k: usize, n: usize,
     let mstridec = stride_multipliers[2];
     let mstridec2 = stride_multipliers[3];
 
-    let mut a = vec![F::zero(); m * k * mstridea[0] * mstridea[1]]; 
-    let mut b = vec![F::zero(); k * n * mstrideb[0] * mstrideb[1]];
-    let mut c1 = vec![F::nan(); m * n * mstridec[0] * mstridec[1]];
-    let mut c2 = vec![F::nan(); m * n * mstridec2[0] * mstridec2[1]];
+    let mut a = vec![F::zero_in(); m * k * mstridea[0] * mstridea[1]]; 
+    let mut b = vec![F::zero_in(); k * n * mstrideb[0] * mstrideb[1]];
+    let mut c1 = vec![F::nan_out(); m * n * mstridec[0] * mstridec[1]];
+    let mut c2 = vec![F::nan_out(); m * n * mstridec2[0] * mstridec2[1]];
 
     for (i, elt) in a.iter_mut().enumerate() {
-        *elt = F::from(i as i64);
+        *elt = F::from_in(i as i64);
     }
     for (i, elt) in b.iter_mut().enumerate() {
-        *elt = F::from(i as i64);
+        *elt = F::from_in(i as i64);
     }
 
     let la = layouts[0];
@@ -439,30 +490,30 @@ fn test_strides_inner<F>(m: usize, k: usize, n: usize,
         // C1 = A B
         F::gemm(
             m, k, n,
-            F::from(1),
+            F::from_out(1),
             a.as_ptr(), rs_a, cs_a,
             b.as_ptr(), rs_b, cs_b,
-            F::zero(),
+            F::zero_out(),
             c1.as_mut_ptr(), rs_c1, cs_c1,
         );
-        
+
         // C1 += 2 A B
         F::gemm(
             m, k, n,
-            F::from(2),
+            F::from_out(2),
             a.as_ptr(), rs_a, cs_a,
             b.as_ptr(), rs_b, cs_b,
-            F::from(1),
+            F::from_out(1),
             c1.as_mut_ptr(), rs_c1, cs_c1,
         );
 
         // C2 = 3 A B 
         F::gemm(
             m, k, n,
-            F::from(3),
+            F::from_out(3),
             a.as_ptr(), rs_a, cs_a,
             b.as_ptr(), rs_b, cs_b,
-            F::zero(),
+            F::zero_out(),
             c2.as_mut_ptr(), rs_c2, cs_c2,
         );
     }
@@ -488,7 +539,7 @@ fn test_strides_inner<F>(m: usize, k: usize, n: usize,
         let irem = index % rs_c1 as usize;
         let jrem = index % cs_c1 as usize;
         if irem != 0 && jrem != 0 {
-            assert!(elt.is_nan(),
+            assert!(F::is_nan_out(*elt),
                 "Element at index={} ({}, {}) should be NaN, but was {}\n\
                 c1: {:?}\n",
             index, i, j, elt,

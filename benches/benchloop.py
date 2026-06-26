30965a71acc7a51212c346475f53ed3a5085397c
Ulrik Sverdrup <ulrik.sverdrup@gmail.com>
2026-06-26 13:09:49 +0200

    bench: Support layout change (i.e fcf vs fcc)

--- benches/benchloop.py
+++ benches/benchloop.py
@@ -6,6 +6,7 @@ Benchmarking script. See --help for details. Compiles benchmark, runs and output
 
 import argparse
 import os
+import re
 import subprocess
 import sys
 import time
@@ -24,6 +25,12 @@ _DEFAULT_FEATURES = "constconf cgemm".split()
 _EXEC = "./target/release/examples/benchmark"
 
 
+def layout_type(value):
+    if not re.match(r"^[fc]{3}$", value):
+        raise argparse.ArgumentTypeError(f"Invalid layout: {value}. Must be [fc]{{3}}, e.g fcc.")
+    return value
+
+
 def bench_loop(args, *, file):
     extra_header = ",nc,kc,mc,threads"
     print("m,k,n,layout,type,average_ns,minimum_ns,median_ns,samples,gflops", extra_header, sep="", file=file)
@@ -32,13 +39,14 @@ def bench_loop(args, *, file):
     mcs = [None] if args.mc is None else args.mc
     for threads in args.threads:
         for ty in args.type:
-            for nc in ncs:
-                for kc in kcs:
-                    for mc in mcs:
-                        bench_iteration(args.size, ty, nc, kc, mc, threads=threads, file=file, sleep=args.sleep)
+            for layout in args.layout:
+                for nc in ncs:
+                    for kc in kcs:
+                        for mc in mcs:
+                            bench_iteration(args.size, ty, nc, kc, mc, layout=layout, threads=threads, file=file, sleep=args.sleep)
 
 
-def bench_iteration(sizes, ty, nc, kc, mc, *, threads, file, sleep):
+def bench_iteration(sizes, ty, nc, kc, mc, *, layout, threads, file, sleep):
     features = list(_DEFAULT_FEATURES)
     if threads > 0:
         features.append("threading")
@@ -61,7 +69,7 @@ def bench_iteration(sizes, ty, nc, kc, mc, *, threads, file, sleep):
     for size in sizes:
         argv = [_EXEC]
         argv.extend(["--type", ty])
-        argv.extend(["--csv", "--layout", "fcc", "--extra-column", extra_column])
+        argv.extend(["--csv", "--layout", layout, "--extra-column", extra_column])
         argv.extend([str(size)] * 3)
 
         print("Running", " ".join(argv), file=sys.stderr)
@@ -79,6 +87,8 @@ def main():
     parser.add_argument("--mc", type=str, nargs="+", help="Sizes or size ranges like 16 or 16:64:8")
     parser.add_argument("--threads", type=int, default=[0], nargs="+",
                         help="Thread use. 0: not enabled; 1: enabled but one thread; n: enabled with n threads.")
+    parser.add_argument("--layout", type=layout_type, default=["fcc"], nargs="+",
+                        help="Layout (f/c combos, e.g. fcc, fff)")
     parser.add_argument("--sleep", type=int, default=1, help="Time to wait between every run")
     parser.add_argument("--output", type=str, default=None, help="Output file (csv format)")
     args = parser.parse_args()
